3f8ec769dfbe69a1a77557b8a40cd58e3f620074
Andrea Formica <andrea.formica@cern.ch>
2012-11-07 14:42:33 +0100

    small change.

--- src/atlas/jetty/test/JdbcHandler.java
+++ src/atlas/jetty/test/JdbcHandler.java
@@ -82,7 +82,7 @@ public class JdbcHandler extends AbstractHandler {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println("<h1>Hello World, this is my JdbcHandler</h1>");
 	}
 
 }
